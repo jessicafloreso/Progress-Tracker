1e993461ba649dba9b1522e6b4139c01c98315af
Ryan Okamoto <rokam475@gmail.com>
2023-03-01 15:34:29 -0600

    everything but exceptions for MVP

--- PokemonTrackerProject/src/DAO/CollectedInterface.java
+++ PokemonTrackerProject/src/DAO/CollectedInterface.java
@@ -11,9 +11,9 @@ import customExceptions.MaxLevelException;
 
 public interface CollectedInterface {
 	public void setConnection()  throws FileNotFoundException, ClassNotFoundException, IOException, SQLException;
-	public List<Collected> getAllCollected();
-	public Optional<Collected> getPokemon(String pokemonName);
-	public boolean catchPokemon(String pokemon, int level) throws MaxLevelException;
-	public boolean levelUp( String pokemon, int level );
+	public List<Collected> getAllCollected(String user);
+	public Optional<Collected> getPokemon(String user, String pokemonName);
+	public boolean catchPokemon(String user, String pokemon, int level) throws MaxLevelException;
+	public boolean levelUp(String user, String pokemon, int level ) throws MaxLevelException;
 	
 }
